425e59af76c5deb34ab4138950910f8dbe6a4a6e
joeskib <146056538+sdfsdfasdfsdfasdfsdfasdf@users.noreply.github.com>
2024-12-16 13:10:23 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 
 
 var sitename = "Supernova"; // Change this to change the name of your website.
-var subtext = "patrick"; // set the subtext
+var subtext = "bored i see?"; // set the subtext
 
 // more settings in main.css
 
